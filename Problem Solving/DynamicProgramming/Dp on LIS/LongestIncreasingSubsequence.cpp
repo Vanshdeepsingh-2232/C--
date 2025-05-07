5b1fec36f3ec349d316df2b36fd36aeb232cbfe9
Vanshdeepsingh-2232 <129372090+Vanshdeepsingh-2232@users.noreply.github.com>
2025-05-07 23:54:18 +0530

    Dp on LIS

---
+++ Problem Solving/DynamicProgramming/Dp on LIS/LongestIncreasingSubsequence.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <stack>
+using namespace std;
+
+// class Solution
+// {
+// private:
+//     int solve(vector<int> &nums, int i, int prev, vector<vector<int>> &dp)
+//     {
+//         if (i == nums.size())
+//             return 0;
+
+//         if (dp[i][prev + 1] != -1)
+//             return dp[i][prev + 1];
+
+//         int take = 0;
+//         if (prev == -1 || nums[i] > nums[prev])
+//         {
+//             take = 1 + solve(nums, i + 1, i, dp);
+//         }
+//         int nontake = 0;
+//         nontake = 0 + solve(nums, i + 1, prev, dp);
+
+//         return dp[i][prev + 1] = max(take, nontake);
+//     }
+
+// public:
+//     int lengthOfLIS(vector<int> &nums)
+//     {
+//         int n = nums.size();
+//         vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+
+//         return solve(nums, 0, -1, dp);
+//     }
+// };
+
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> pre(n + 1, 0);
+        vector<int> curr(n + 1, 0);
+
+        for (int i = n - 1; i >= 0; i--)
+        {
+            for (int prev = i - 1; prev >= -1; prev--)
+            {
+                int len = 0;
+                if (prev == -1 || nums[i] > nums[prev])
+                    len = 1 + pre[i + 1];
+                len = max(len, 0 + pre[prev + 1]);
+                curr[prev + 1] = len;
+            }
+
+            pre = curr;
+        }
+
+        return pre[-1 + 1];
+    }
+};
+
+int main()
+{
+    vector<int> v = {10, 9, 2, 5, 3, 7, 101, 18};
+    Solution s1;
+    cout << s1.lengthOfLIS(v);
+    return 0;
+}
