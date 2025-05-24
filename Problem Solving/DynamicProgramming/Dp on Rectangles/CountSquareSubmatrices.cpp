8623a87cfe04a966f4a6b76e0ed658418bfb678e
Vanshdeepsingh-2232 <129372090+Vanshdeepsingh-2232@users.noreply.github.com>
2025-05-25 00:36:38 +0530

    Dp on squares

---
+++ Problem Solving/DynamicProgramming/Dp on Rectangles/CountSquareSubmatrices.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <set>
+#include <stack>
+using namespace std;
+
+class Solution
+{
+public:
+    int countSquares(vector<vector<int>> &matrix)
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++)
+            dp[i][0] = matrix[i][0];
+        for (int j = 0; j < m; j++)
+            dp[0][j] = matrix[0][j];
+
+        int ans = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j < m; j++)
+            {
+                if (matrix[i][j] == 0)
+                    dp[i][j] = 0;
+                else
+                    dp[i][j] = 1 + min(dp[i - 1][j - 1], min(dp[i - 1][j], dp[i][j - 1]));
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                ans += dp[i][j];
+            }
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<vector<int>> matrix = {{0, 1, 1, 1},
+                                  {1, 1, 1, 1},
+                                  {0, 1, 1, 1}};
+    Solution s1;
+    cout << s1.countSquares(matrix);
+    return 0;
+}
